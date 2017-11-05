9b1b0d328448c7c66596cfb0a66043b9a2b27d53
ifmuser1 <mehdow@gmail.com>
2017-11-06 10:14:46 +1100

    added page image style correction js

--- www/js/index.js
+++ www/js/index.js
@@ -20,6 +20,8 @@ var app = {
     },
     
     receivedEvent: function(id) {
+        
+        alignOrgPageImage();
         //webViewerRef = cordova.InAppBrowser.open("http://afm.integratedfm.com.au/", '_blank',  'location=yes');
         
         //window.open("http://afm.integratedfm.com.au/", '_self');//self system
@@ -34,3 +36,17 @@ var app = {
 function onImageClicked(){
     window.open("https://ecusis.ecu.edu.au/", '_system');
 }
+
+function alignOrgPageImage(){
+	var org_page_image=null;
+	org_page_image=$("#org-page-image-place-holder")[0];
+	if (org_page_image==null){
+		return;
+	}
+    
+	
+	var image_left = $.mobile.window.width() / 2 - org_page_image.width/2;//org_page_image.width/2;
+	$(".image-custom").css('left', image_left);
+	$(".page-sisfm-org").css("background-color",stored_msg.screen_color);
+	
+};
